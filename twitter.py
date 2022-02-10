e2cce67eb4e7845af52f33eaa4f0ddcad5f1eea6
SeokJingu <98717916+SeokJingu@users.noreply.github.com>
2022-02-10 12:31:00 +0900

    Add files via upload

--- twitter.py
+++ twitter.py
@@ -1,5 +1,13 @@
 #from more_itertools import consumer
+from ast import Num
+from tempfile import tempdir
+from isort import file
 import tweepy
+import random
+import time
+from datetime import datetime
+
+
 
 def connect_twitter():
     f = open("twitter_api.txt","r")
@@ -25,21 +33,33 @@ def connect_twitter():
     string = f.readline()
     client_secret = string.strip()
     f.close()
-#    client = tweepy.Client(
-#        consumer_key = api_key , consumer_secret=api_secret , 
-#        access_token = access_token , access_token_secret=access_token_secret
-#        )
-
-#    auth = tweepy.OAuth1UserHandler(consumer_key=api_key, consumer_secret=api_secret)
-#    auth.set_access_token(access_token, access_token_secret)
-#    api = tweepy.API(auth)
     auth = tweepy.OAuthHandler(consumer_key = api_key, consumer_secret = api_secret)
     auth.set_access_token(access_token, access_token_secret)
-    
     api = tweepy.API(auth)
     return api
 
-api = connect_twitter()
-status = "bot test."
-api.update_status(status)
-api.update_status_with_media(status,"twitter_doc/1.jpg")
+def read_doc(file_name):
+    file = open("twitter_doc/"+str(file_name)+".txt","r", encoding='utf-8')
+    status = file.read()
+    file.close()
+    return status
+
+def twitter_post():
+    api = connect_twitter()
+    random_num = random.randrange(1,19)
+    doc_num = str(random_num)
+    status = read_doc(doc_num)
+    picture_file = "twitter_doc/"+doc_num+".jpg"
+    api.update_status_with_media(status,picture_file)
+
+def main():
+    current_time = datetime.now()
+    old_hour = current_time.minute
+    while True:
+        current_time = datetime.now()
+        if current_time.hour != old_hour :
+            twitter_post()
+        old_hour = current_time.minute
+
+main()
+
